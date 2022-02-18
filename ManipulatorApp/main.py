df31fb51eb7a0ee54ce076dfcd9499568c228e26
Daniel Sziling <97255726+Szezi@users.noreply.github.com>
2022-02-18 21:17:42 +0100

    Functions write to and read from file added.

--- ManipulatorApp/main.py
+++ ManipulatorApp/main.py
@@ -1,5 +1,38 @@
 import trajectory
 import os
+import datetime as dt
+
+
+def write_generated_path_to_file(array: list, file_path: str, file_name: str, file_format: str):
+    path = trajectory.path_generator(array)
+    path_data = path[0].copy()
+    path_status = path[1]
+
+    file_date = str(dt.date.today()) + '_' + dt.datetime.now().strftime("%H.%M.%S")
+
+    try:
+        with open(os.path.join(file_path, file_name + file_date + file_format), 'w', encoding='utf-8') as file:
+            file.write("['command', theta0, theta1, theta2, theta3, time]" + '\n')
+            for element in path_data:
+                file.write(str(element) + '\n')
+            file.write(path_status)
+    except:
+        print('Sth went wrong during writing to file')
+
+
+def read_generated_path_from_file(file_path: str):
+    path_from_file = []
+    try:
+        with open(os.path.join(file_path), 'r', encoding='utf-8') as file:
+            path = [line.rstrip() for line in file]
+            for elem in path[:-1]:
+                element = elem.split(', ')
+                element[0] = element[0][2:-1]
+                element[-1] = element[-1][:-1]
+                path_from_file.append(element)
+    except:
+        print('Sth went wrong during reading the file')
+    return path_from_file
 
 if __name__ == '__main__':
     # [command, traj, config, end_points, time]
@@ -8,18 +41,13 @@ if __name__ == '__main__':
                   ['home', 'cp_linear', 'config_1', [0, 0, 460, 90], 3.0, [54, 0]],
                   ['wait', '', '', [0, 0, 0, 0], 10.0, [54, 0]]]
 
-    test_path = trajectory.path_generator(test_array)
+    file_path_write = r'C:\Users\SZILING\Desktop\ManipulatorApp\data'
+    file_name_write = 'generated_path_'
+    file_format_write = '.txt'
 
-    test_path_data = test_path[0].copy()
-    test_path_status = test_path[1]
+    # write_generated_path_to_file(test_array, file_path_write, file_name_write, file_format_write)
 
-    file_path = r'C:\Users\SZILING\Desktop\ManipulatorApp\data'
+    file_path_read = r'C:\Users\SZILING\Desktop\ManipulatorApp\data\test_path.txt'
 
-    try:
-        with open(os.path.join(file_path, 'test_path.txt'), 'w', encoding='utf-8') as f:
-            f.write('[command, [theta0, theta1, theta2, theta3], time]' + '\n')
-            for element in test_path_data:
-                f.write(str(element) + '\n')
-            f.write(test_path_status)
-    except:
-        print('Sth went wrong')
+    read_path = read_generated_path_from_file(file_path_read)
+    print(read_path)
