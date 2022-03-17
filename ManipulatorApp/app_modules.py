93f0d232586105c96d704c0d0b35695a96a7aca4
Daniel Sziling <97255726+Szezi@users.noreply.github.com>
2022-03-17 20:08:55 +0100

    Initial commit. Import modules.

---
+++ ManipulatorApp/app_modules.py
@@ -0,0 +1,8 @@
+# GUI FILE
+from ManipulatorApp.ui.ui_ManipulatorApp import Ui_MainWindow
+
+# IMPORT FUNCTIONS
+from ManipulatorApp.ui.ui_functions import *
+
+# ==> APP FUNCTIONS
+from ManipulatorApp.ui.app_functions import *
