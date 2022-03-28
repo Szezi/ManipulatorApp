dec3fecfe0a716bc64395357d14772b764f4c730
Daniel Sziling <97255726+Szezi@users.noreply.github.com>
2022-03-28 20:42:38 +0200

    Change communication class object to script

--- ManipulatorApp/modules/communication.py
+++ ManipulatorApp/modules/communication.py
@@ -1,39 +1,63 @@
 from pyfirmata2 import Arduino, util
 
+try:
+    board = Arduino(Arduino.AUTODETECT)
+    board.samplingOn(10)
+    print(board)
+    iter8 = util.Iterator(board)
+    iter8.start()
 
-class RobotComm(object):
-    """ The class establishes communication with robotic arm and read from / write to robotic servos """
-    def __init__(self, pin_s1: str, pin_s2: str, pin_s3: str, pin_s4: str, pin_s5: str, pin_s6: str):
-        self.board = Arduino(Arduino.AUTODETECT)
-        self.board.samplingOn(10)
-        self.iter8 = util.Iterator(self.board)
+    pin_s1 = 'd:8:s'
+    pin_s2 = 'd:9:s'
+    pin_s3 = 'd:10:s'
+    pin_s4 = 'd:11:s'
+    pin_s5 = 'd:12:s'
+    pin_s6 = 'd:13:s'
 
-        self.servo1 = self.board.get_pin(pin_s1)  # servo base
-        self.servo2 = self.board.get_pin(pin_s2)  # servo link 1
-        self.servo3 = self.board.get_pin(pin_s3)  # servo link 2
-        self.servo4 = self.board.get_pin(pin_s4)  # servo link 3
-        self.servo5 = self.board.get_pin(pin_s5)  # servo effector_movement
-        self.servo6 = self.board.get_pin(pin_s6)  # servo effector_action
+    servo1 = board.get_pin(pin_s1)
+    servo2 = board.get_pin(pin_s2)
+    servo3 = board.get_pin(pin_s3)
+    servo4 = board.get_pin(pin_s4)
+    servo5 = board.get_pin(pin_s5)
+    servo6 = board.get_pin(pin_s6)
 
-    def start(self):
-        self.iter8.start()
 
-    def stop(self):
-        self.board.exit()
+    def servos_write_initial(s1, s2, s3, s4, s5, s6):
+        servo1.write(int(s1))
+        servo2.write(int(s2))
+        servo3.write(int(s3))
+        servo4.write(int(s4))
+        servo5.write(int(s5))
+        servo6.write(int(s6))
 
-    def servos_write(self, s1, s2, s3, s4, s5, s6):
-        self.servo1.write(int(s1))
-        self.servo2.write(int(s2))
-        self.servo3.write(int(s3))
-        self.servo4.write(int(s4))
-        self.servo5.write(int(s5))
-        self.servo6.write(int(s6))
 
-    def servos_read(self):
-        s1 = self.servo1.read()
-        s2 = self.servo2.read()
-        s3 = self.servo3.read()
-        s4 = self.servo4.read()
-        s5 = self.servo5.read()
-        s6 = self.servo6.read()
+    servos_write_initial(90, 60, 0, 180, 90, 90)
+
+
+    def servos_write(s1, s2, s3, s4, s5, s6):
+        servo1.write(int(s1))
+        servo2.write(int(s2))
+        servo3.write(int(s3))
+        servo4.write(int(s4))
+        servo5.write(int(s5))
+        servo6.write(int(s6))
+
+
+    def servos_read():
+        s1 = servo1.read()
+        s2 = servo2.read()
+        s3 = servo3.read()
+        s4 = servo4.read()
+        s5 = servo5.read()
+        s6 = servo6.read()
         return s1, s2, s3, s4, s5, s6
+
+except:
+    status_log = 'Communication: Failed'
+    status = False
+
+else:
+    status_log = 'Communication: Success'
+    status = True
+
+print(status_log)
