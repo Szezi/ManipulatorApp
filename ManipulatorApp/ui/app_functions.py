ed3684bfe7005728fe678137cfa0fdb75b384f2a
Daniel Sziling <97255726+Szezi@users.noreply.github.com>
2022-05-08 20:45:13 +0200

    Add comments

--- ManipulatorApp/ui/app_functions.py
+++ ManipulatorApp/ui/app_functions.py
@@ -19,7 +19,10 @@ class AppFunctions(MainWindow):
         self.stop = None
 
     def page_fk(self):
+        # Check the effector dimensions
         eff = UIFunctions.effector_check(self)
+
+        # Take values from sliders and calculate forward kinematics
         theta1 = self.ui.horizontalSlider_fk_s1.value()
         theta2 = self.ui.horizontalSlider_fk_s1_2.value()
         theta3 = self.ui.horizontalSlider_fk_s1_3.value()
@@ -27,17 +30,23 @@ class AppFunctions(MainWindow):
 
         result = FK.fk_dh(theta1, theta2, theta3, theta4, eff)
 
+        # Display the result of calculation
         self.ui.lcdNumber_dh_X_2.display(result[1][0])
         self.ui.lcdnumber_dh_Y_2.display(result[1][1])
         self.ui.lcdnumber_dh_Z_2.display(result[1][2])
+
+        # Print the result
         UIFunctions.log_list(self, 'Page FK: ' + result[5])
 
-        # Sim
+        # Simulate the robotic arm with given parameters
         self.ui.MplWidget_fk.mpl_draw(result[4], result[3], result[2], result[1])
         self.ui.MplWidget_fk.canvas.flush_events()
 
     def page_ik(self):
+        # Check the effector dimensions
         eff = UIFunctions.effector_check(self)
+
+        # Take values from sliders and calculate inverse kinematics
         value_x = self.ui.horizontalSlider_ik_x.value()
         value_y = self.ui.horizontalSlider_ik_y.value()
         value_z = self.ui.horizontalSlider_ik_z.value()
@@ -46,8 +55,11 @@ class AppFunctions(MainWindow):
         result = IK.ik_geo(value_x, value_y, value_z, value_alpha, eff)
         result_config1 = result[0]
         result_config2 = result[1]
+
+        # Print the result
         UIFunctions.log_list(self, 'Page IK: ' + result[2][0] + " " + result[2][1] + " " + result[2][2])
 
+        # Display the result of calculation
         self.ui.lcdNumber_ik_s1.display((result_config1[0]))
         self.ui.lcdNumber_ik_s2.display((result_config1[1]))
         self.ui.lcdNumber_ik_s3.display((result_config1[2]))
@@ -58,7 +70,7 @@ class AppFunctions(MainWindow):
         self.ui.lcdNumber_ik_s3_2.display((result_config2[2]))
         self.ui.lcdNumber_ik_s4_2.display((result_config2[3]))
 
-        # Sim
+        # Simulate the robotic arm with given parameters
         if self.ui.tabWidget_4.currentIndex() == 0:
             calculated = FK.fk_dh(result_config1[0], result_config1[1], result_config1[2], result_config1[3], eff)
             self.ui.MplWidget_ik.mpl_draw(calculated[4], calculated[3], calculated[2], calculated[1])
@@ -67,7 +79,10 @@ class AppFunctions(MainWindow):
             self.ui.MplWidget_ik.mpl_draw(calculated[4], calculated[3], calculated[2], calculated[1])
 
     def page_manual(self):
+        # Check the effector dimensions
         eff = UIFunctions.effector_check(self)
+
+        # Set values of sliders in different control modes
         if self.ui.tab_manual_joints.isVisible():
             value_j1 = self.ui.horizontalSlider_j_s1.value()
             value_j2 = self.ui.horizontalSlider_j_s2.value()
@@ -75,11 +90,15 @@ class AppFunctions(MainWindow):
             value_j4 = self.ui.horizontalSlider_j_s4.value()
 
             value_manual_joints = FK.fk_dh(int(value_j1), int(value_j2), int(value_j3), int(value_j4), eff)
-            UIFunctions.log_list(self, 'Manual mode: ' + value_manual_joints[5])
+
+            # Set values of sliders
             self.ui.horizontalSlider_manual_x.setValue(value_manual_joints[1][0])
             self.ui.horizontalSlider_manual_y.setValue(value_manual_joints[1][1])
             self.ui.horizontalSlider_manual_z.setValue(value_manual_joints[1][2])
             self.ui.verticalSlider_manual_orient.setValue(value_manual_joints[0])
+
+            # Set the status with parameters sending to robotic arm
+            status = value_manual_joints[5]
 
         elif self.ui.tab_manual_xyz.isVisible():
             value_x = self.ui.horizontalSlider_manual_x.value()
@@ -88,9 +107,11 @@ class AppFunctions(MainWindow):
             value_alpha = self.ui.verticalSlider_manual_orient.value()
 
             value_manual_xyz = IK.ik_geo(value_x, value_y, value_z, value_alpha, eff)
-            UIFunctions.log_list(self, 'Manual mode: ' + value_manual_xyz[2][0] + " " + value_manual_xyz[2][1] + " " +
-                                 value_manual_xyz[2][2])
 
+            # Set the status with parameters sending to robotic arm
+            status = value_manual_xyz[2][0] + " " + value_manual_xyz[2][1] + " " + value_manual_xyz[2][2]
+
+            # Set values of sliders
             if self.ui.comboBox_auto_config_2.currentIndex() == 0:
                 index = 0
             elif self.ui.comboBox_auto_config_2.currentIndex() == 1:
@@ -103,6 +124,10 @@ class AppFunctions(MainWindow):
             self.ui.horizontalSlider_j_s3.setValue(value_manual_xyz[index][2])
             self.ui.horizontalSlider_j_s4.setValue(value_manual_xyz[index][3])
 
+        else:
+            status = 'Something went wrong'
+
+        # Control robotic arm with given parameters
         servo_1 = self.ui.horizontalSlider_j_s1.value()
         servo_2 = self.ui.horizontalSlider_j_s2.value()
         servo_3 = self.ui.horizontalSlider_j_s3.value()
@@ -114,14 +139,16 @@ class AppFunctions(MainWindow):
             comm.servos_write(servo_1, servo_2, servo_3, servo_4, servo_5, servo_6)
         except:
             status = 'Error: Robotic Arm not connected'
-            print(status)
-            UIFunctions.log_list(self, 'Manual mode: ' + status)
 
-        # Sim
+        # Print the status
+        UIFunctions.log_list(self, 'Manual mode: ' + status)
+
+        # Simulate the robotic arm with given parameters
         calculated = FK.fk_dh(servo_1, servo_2, servo_3, servo_4, eff)
         self.ui.MplWidget_ik_2.mpl_draw(calculated[4], calculated[3], calculated[2], calculated[1])
 
     def page_automatic(self):
+        # Buttons functions
         self.ui.btn_auto_Add_path_gen.clicked.connect(lambda: UIFunctions.generate_path(self))
         self.ui.btn_auto_Add_path_read.clicked.connect(lambda: UIFunctions.read_path(self))
         self.ui.btn_auto_init.clicked.connect(lambda: AppFunctions.automatic_mode_init(self))
@@ -132,36 +159,54 @@ class AppFunctions(MainWindow):
         self.ui.btn_auto_next.clicked.connect(lambda: AppFunctions.automatic_step_next(self))
 
     def automatic_stop(self):
+        # Stopping executing robotic program
         self.stop = True
         status = 'Stop robotic program'
         UIFunctions.log_list(self, status)
 
     def automatic_start(self):
+        # Start executing robotic program
         self.stop = False
+
+        # Check the selected mode
         if self.ui.comboBox_auto_sim_servo.currentText() == 'SIM':
+            # Execute program in simulation
             status = 'Executing robotic program - SIM'
             while not self.stop:
+                # Take next line of robotic program
                 step_values_next = self.robotic_arm.next()
+
+                # Take effector dimensions
                 eff1 = step_values_next[15][1:]
                 eff2 = step_values_next[16][:1]
                 eff = [int(eff1), int(eff2)]
                 print(step_values_next)
+
+                # Calculate forward kinematics
                 calculated = FK.fk_dh(float(step_values_next[8]),
                                       float(step_values_next[9]),
                                       float(step_values_next[10]),
                                       float(step_values_next[11]), eff)
+
+                # Simulate the robotic arm with given parameters
                 self.ui.MplWidget_auto.mpl_draw(calculated[4], calculated[3], calculated[2], calculated[1])
                 QApplication.processEvents()
                 time.sleep(float(step_values_next[14]))
         else:
+            # Execute program with robotic arm
             status = 'Executing robotic program - SERVO'
-            #     Send to servos
+            # Control robotic arm with given parameters
             # while not self.stop:
+            # Take next line of robotic program
             #     step_values_next = self.robotic_arm.next()
+
+            # Take effector dimensions
             #     eff1 = step_values_next[15][1:]
             #     eff2 = step_values_next[16][:1]
             #     eff = [int(eff1), int(eff2)]
             #     print(step_values_next)
+
+            #     Control robotic arm with given parameters
             #     comm.servos_write(float(step_values_next[8]),
             #                       float(step_values_next[9]),
             #                       float(step_values_next[10]),
@@ -171,27 +216,43 @@ class AppFunctions(MainWindow):
             #     QApplication.processEvents()
             #     time.sleep(float(step_values_next[14]))
 
+        # Print the status
         UIFunctions.log_list(self, status)
 
     def automatic_step_next(self):
+        # Execute one next line of robotic program
         try:
+            # Take next line of robotic program
             step_values_next = self.robotic_arm.next()
+
+            # Take effector dimensions
             eff1 = step_values_next[15][1:]
             eff2 = step_values_next[16][:1]
             eff = [int(eff1), int(eff2)]
             print(step_values_next)
+
+            # Check the selected mode
             if self.ui.comboBox_auto_sim_servo.currentText() == 'SIM':
+                # Execute program in simulation
                 status = 'Step forward SIM'
+
+                # Calculate forward kinematics
                 calculated = FK.fk_dh(float(step_values_next[8]),
                                       float(step_values_next[9]),
                                       float(step_values_next[10]),
                                       float(step_values_next[11]), eff)
+
+                # Simulate the robotic arm with given parameters
                 self.ui.MplWidget_auto.mpl_draw(calculated[4], calculated[3], calculated[2], calculated[1])
             else:
+                # Execute program with robotic arm
                 status = 'Step forward SERVO'
-                #     Send to servos
+
+                #     # Send to servos
                 #     step_values_next = self.robotic_arm.next()
                 #     print(step_values_next)
+
+                #     Control robotic arm with given parameters
                 #     comm.servos_write(float(step_values_next[8]),
                 #                       float(step_values_next[9]),
                 #                       float(step_values_next[10]),
@@ -202,27 +263,41 @@ class AppFunctions(MainWindow):
         except AttributeError:
             status = 'Automatic mode not initialized'
 
+        # Print the status
         UIFunctions.log_list(self, status)
 
     def automatic_step_prev(self):
+        # Execute one previous line of robotic program
         try:
+            # Take previous line of robotic program
             step_values_prev = self.robotic_arm.prev()
+
+            # Take effector dimensions
             eff1 = step_values_prev[15][1:]
             eff2 = step_values_prev[16][:1]
             eff = [int(eff1), int(eff2)]
             print(step_values_prev)
+
             if self.ui.comboBox_auto_sim_servo.currentText() == 'SIM':
+                # Execute program in simulation
                 status = 'Step backward SIM'
+
+                # Calculate forward kinematics
                 calculated = FK.fk_dh(float(step_values_prev[8]),
                                       float(step_values_prev[9]),
                                       float(step_values_prev[10]),
                                       float(step_values_prev[11]), eff)
+
+                # Simulate the robotic arm with given parameters
                 self.ui.MplWidget_auto.mpl_draw(calculated[4], calculated[3], calculated[2], calculated[1])
             else:
+                # Execute program with robotic arm
                 status = 'Step backward SERVO'
                 #     Send to servos
                 #     step_values_next = self.robotic_arm.next()
                 #     print(step_values_next)
+
+                #     Control robotic arm with given parameters
                 #     comm.servos_write(float(step_values_next[8]),
                 #                       float(step_values_next[9]),
                 #                       float(step_values_next[10]),
@@ -233,6 +308,7 @@ class AppFunctions(MainWindow):
         except AttributeError:
             status = 'Automatic mode not initialized'
 
+        # Print the status
         UIFunctions.log_list(self, status)
 
     def automatic_mode_init(self):
@@ -246,19 +322,23 @@ class AppFunctions(MainWindow):
             self.robotic_path = self.read_robotic_path
             robotic_path_copy = self.robotic_path.copy()
 
+            # Executing robotic arm in loop
             repeat = self.ui.spinBox.value()
             for _ in range(1, repeat):
                 self.robotic_path.extend(robotic_path_copy)
 
+            # Create robotic arm program
             self.robotic_arm = trajectory.RoboticMove(self.robotic_path)
 
             status = 'Automatic mode initialized'
         else:
             status = ' Installed effector is different as in robotic path'
 
+        # Print the status
         UIFunctions.log_list(self, status)
 
     def automatic_mode_break(self):
+        # Delete initialized robotic_arm object
         try:
             if self.robotic_arm is None:
                 pass
@@ -267,14 +347,16 @@ class AppFunctions(MainWindow):
                 del self.robotic_arm
         except AttributeError:
             status = 'Automatic mode not initialized'
+
+            # Print the status
             UIFunctions.log_list(self, status)
 
     def page_settings(self):
-        # Effector
         AppFunctions.effector_tab(self)
         AppFunctions.communication_tab(self)
 
     def effector_tab(self):
+        # Button functions in effector tab in settings page
         self.ui.btn_kal2_set_1.clicked.connect(lambda: UIFunctions.effector_calibration(self, 103, 0))
         self.ui.btn_kal2_set_1.clicked.connect(
             lambda: UIFunctions.log_list(self, 'Effector dim. changed to grippers'))
@@ -289,6 +371,7 @@ class AppFunctions(MainWindow):
             True) if self.ui.checkBox_kal2_ef.checkState() else self.ui.radioButton_home_effector.setChecked(False))
 
     def communication_tab(self):
+        # Set status of communication green/red
         if comm.status:
             self.ui.radioButton_home_comm.setChecked(True)
             self.ui.radioButton_home_comm_2.setChecked(True)
@@ -297,6 +380,7 @@ class AppFunctions(MainWindow):
             self.ui.radioButton_home_comm_2.setChecked(False)
 
     def communication_radiobutton(self):
+        # Set/check the communication with robotic arm (Arduino)
         try:
             if not self.ui.radioButton_home_comm_2.isChecked():
                 comm.board.exit()
@@ -310,6 +394,8 @@ class AppFunctions(MainWindow):
                     port = 'No connection'
 
                 self.ui.label_home_port_2.setText('Communication on port:' + port)
+
+                # Print the status
                 UIFunctions.log_list(self, comm.status_log)
         except:
             self.ui.label_home_port_2.setText('Communication on port: No connection/Disconnected')
