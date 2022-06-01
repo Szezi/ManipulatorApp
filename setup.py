9a1e37ee2d7cf2fcdfafec16cb36eb3d58225b6c
Daniel Sziling <97255726+Szezi@users.noreply.github.com>
2022-06-01 20:45:00 +0200

    feat: create setup.py file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='ManipulatorApp',
+    version='1.0',
+    url='https://github.com/Szezi/ManipulatorApp',
+    license='GNU General Public License v3.0',
+    author='Daniel Sziling',
+    description='Desktop application to control robotic arm',
+)
